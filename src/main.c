c571e59bc36a201d8df3b7c7d4f35e9202eb54b6
william <wiliberm@gmail.com>
2019-06-29 19:15:58 +0300

    POC of loading a library

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <dlfcn.h>
 #include "fib.h"
+
+void (*print_hello)(void);
 
 int main()
 {
+	void * dl = dlopen("./lib.so", RTLD_LAZY);
+	print_hello = dlsym(dl, "print_hello");
+	(*print_hello)();
 	printf("Fib(8) = %d\n", fib(8));
 	return 0;
 }
