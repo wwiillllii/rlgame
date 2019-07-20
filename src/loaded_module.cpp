452203e3aa5671c3e5f683610cac8d07d6e4ac07
william <wiliberm@gmail.com>
2019-07-20 22:26:45 +0300

    Implemented most of loader functionality

--- src/loaded_module.cpp
+++ src/loaded_module.cpp
@@ -1,1 +1,25 @@
 #include "loaded_module.h"
+
+#ifdef __linux__
+
+LoadedModule::LoadedModule(std::string path) :
+	handle(dlopen(path.c_str(), RTLD_GLOBAL)),
+	path(path)
+{}
+
+LoadedModule::~LoadedModule(){
+	if (this->handle != nullptr){
+		dlclose(this->handle);
+		this->handle = nullptr;
+	}
+}
+
+bool LoadedModule::isLoaded() const{
+	return this->handle != nullptr;
+}
+
+std::string LoadedModule::getPath() const{
+	return this->path;
+}
+
+#endif 
--- src/loader.cpp
+++ src/loader.cpp
@@ -1,1 +1,25 @@
 #include "loader.h"
+#include "directory.h"
+
+#ifdef __linux__
+
+#include <sys/types.h>
+#include <dirent.h>
+
+Loader::Loader() :
+	loaded_modules()
+{}
+
+Loader::~Loader(){}
+
+void Loader::loadFromDir(std::string dir_path){
+
+}
+
+size_t Loader::loadedModuleCount() const{
+	return this->loaded_module.size();
+}
+
+#else
+#error Unsupported OS! (Or you are not compiling with gcc).
+#endif
