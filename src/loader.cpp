92d36eee7884c6959402bf2879dd42e93fd2ba55
william <wiliberm@gmail.com>
2019-08-11 06:29:19 +0300

    Fixed type in loader.cpp

--- src/loader.cpp
+++ src/loader.cpp
@@ -13,11 +13,11 @@ Loader::Loader() :
 Loader::~Loader(){}
 
 void Loader::loadFromDir(std::string dir_path){
-
+	// TODO
 }
 
 size_t Loader::loadedModuleCount() const{
-	return this->loaded_module.size();
+	return this->loaded_modules.size();
 }
 
 #else
